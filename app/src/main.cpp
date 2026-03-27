946efcbb8dc789f761e5e90c7c0ce0f801f7d17c
Juanferreyra412 <juanferreyra99@gmail.com>
2026-03-27 15:14:35 -0300

    Tarea 1
    	- Cambio de velocidad de blinky a 200 ms

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,7 +2,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-#define SLEEP_TIME_MS 1000
+#define SLEEP_TIME_MS 200
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED_NODE DT_ALIAS(led0)
@@ -13,18 +13,21 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main(void)
 {
-    bool led_state = true;
+	bool led_state = true;
 
-    if (!gpio_is_ready_dt(&led)) return 0;
+	if (!gpio_is_ready_dt(&led))
+		return 0;
 
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+	if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0)
+		return 0;
 
-    while (1) {
-        if (gpio_pin_toggle_dt(&led) < 0) return 0;
+	while (1) {
+		if (gpio_pin_toggle_dt(&led) < 0)
+			return 0;
 
-        led_state = !led_state;
-        LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(SLEEP_TIME_MS);
-    }
-    return 0;
+		led_state = !led_state;
+		LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
+		k_msleep(SLEEP_TIME_MS);
+	}
+	return 0;
 }
