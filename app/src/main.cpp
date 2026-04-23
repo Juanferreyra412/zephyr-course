ae3850d9bdc49592f5f90716ac01df5bc4791016
Juanferreyra412 <juanferreyra99@gmail.com>
2026-04-22 23:34:40 -0300

    Tarea 3 (Lesson 3 - Task 1)
    	- Agregado archivo app.overlay para definir el LED de la placa
    	- Agregado Kconfig para definir el "heartbeat rate"
    	- Main

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,100 +1,53 @@
 /*
- * Blink LED (L3 - Task 1)
+ * Blink LED (L4 - Task 1)
  *
- * Este programa utiliza el subsistema PWM de Zephyr para controlar el brillo
- * y el parpadeo de un LED. Implementa un efecto de "fade out" (desvanecimiento)
- * basándose en las configuraciones definidas en Kconfig (brillo máximo, tiempo
- * de desvanecimiento y tiempo de parpadeo).
+ * Este programa utiliza el subsistema GPIO de Zephyr para hacer un efecto de "heartbeat" con el
+ * LED. El LED se define en el archivo app.overlay
  */
+
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/pwm.h>
 #include <zephyr/logging/log.h>
 
-#define SLEEP_TIME_MS   CONFIG_APP_LED_BLINK_SLEEP_TIME_MS
-#define LED_BRIGHTNESS  CONFIG_LED_BRIGHTNESS
-#define PWM_PERIOD_MS   10
-#define PWM_PERIOD_NS   PWM_MSEC(PWM_PERIOD_MS)
-#define PWM_PULSE_WIDTH (uint32_t)(LED_BRIGHTNESS * (PWM_PERIOD_NS / 100))
-#define FADE_STEPS      (uint32_t)(CONFIG_LED_FADE_TIME_MS / PWM_PERIOD_MS)
-#define FADE_DECREMENT  (uint32_t)((FADE_STEPS != 0) ? (PWM_PULSE_WIDTH / FADE_STEPS) : 0)
+#define SLEEP_TIME_MS CONFIG_APP_HEARTBEAT_PERIOD_MS
 
-#define PRINT_LOG(X, ...)                                                                          \
-	if (IS_ENABLED(CONFIG_ENABLE_LED_DEBUGGING))                                                   \
-	printk(X, ##__VA_ARGS__)
+/* The devicetree node identifier for the "app-led" alias. */
+#define LED_NODE DT_ALIAS(app_led)
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
-static const struct pwm_dt_spec pwm_led = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
-
 int main(void)
 {
+	printk("Iniciando heartbeat con LED\n");
 
-	if (IS_ENABLED(CONFIG_LED_SUBSYSTEM)) {
-		int err;
-
-		if (!pwm_is_ready_dt(&pwm_led)) {
-			PRINT_LOG("Error: El dispositivo PWM %s no está listo\n", pwm_led.dev->name);
-			return 0;
-		}
-
-		PRINT_LOG("Iniciando PWM...\n");
-		PRINT_LOG("Brillo: %d\n", LED_BRIGHTNESS);
-		PRINT_LOG("Periodo del PWM: %d\n", PWM_PERIOD_NS);
-		PRINT_LOG("Pulso: %d\n", PWM_PULSE_WIDTH);
-		PRINT_LOG("Fade decrement: %d\n", FADE_DECREMENT);
-		PRINT_LOG("Fade steps: %d\n", FADE_STEPS);
-
-		if (!IS_ENABLED(CONFIG_CUSTOM_BLINK_PATTERN)) {
-			while (1) {
-
-				uint32_t pulse_width_current = PWM_PULSE_WIDTH;
-				err = pwm_set_dt(&pwm_led, PWM_PERIOD_NS, PWM_PULSE_WIDTH);
-				if (err) {
-					PRINT_LOG("Error al configurar el PWM (err %d)\n", err);
-					return 0;
-				} else {
-					PRINT_LOG("LED ENCENDIDO\n");
-				}
-
-				k_sleep(K_MSEC(SLEEP_TIME_MS));
-
-				for (uint16_t i = 0; i < FADE_STEPS; i++) {
-					if (i == 0) {
-						PRINT_LOG("Iniciando fading\n");
-					}
-					pulse_width_current -= FADE_DECREMENT;
-					err = pwm_set_dt(&pwm_led, PWM_PERIOD_NS, pulse_width_current);
-					k_sleep(K_MSEC(PWM_PERIOD_MS));
-				}
-
-				pwm_set_dt(&pwm_led, PWM_PERIOD_NS, 0);
-				PRINT_LOG("LED APAGADO\n");
-				k_sleep(K_MSEC(SLEEP_TIME_MS));
-			}
-		} else if (IS_ENABLED(CONFIG_CUSTOM_BLINK_PATTERN)) {
-
-			// Patrón de latido (doble parpadeo)
-
-			PRINT_LOG("Iniciando patrón personalizado\n");
-			while (1) {
-				pwm_set_dt(&pwm_led, PWM_PERIOD_NS, PWM_PULSE_WIDTH);
-				PRINT_LOG("LED ENCENDIDO\n");
-				k_sleep(K_MSEC(100));
-				pwm_set_dt(&pwm_led, PWM_PERIOD_NS, 0);
-				PRINT_LOG("LED APAGADO\n");
-				k_sleep(K_MSEC(100));
-
-				pwm_set_dt(&pwm_led, PWM_PERIOD_NS, PWM_PULSE_WIDTH);
-				PRINT_LOG("LED ENCENDIDO\n");
-				k_sleep(K_MSEC(100));
-				pwm_set_dt(&pwm_led, PWM_PERIOD_NS, 0);
-				PRINT_LOG("LED APAGADO\n");
-
-				k_sleep(K_MSEC(700));
-			}
-		}
+	if (!gpio_is_ready_dt(&led)) {
+		printk("Error: El dispositivo GPIO %s no está listo\n", led.port->name);
+		return 0;
 	}
 
-	printk("Fin del programa\n");
+	if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) {
+		printk("Error: No se pudo configurar el pin GPIO %d\n", led.pin);
+		return 0;
+	}
+
+	while (1) {
+
+		gpio_pin_set_dt(&led, 1);
+		printk("LED ENCENDIDO\n");
+		k_sleep(K_MSEC(100));
+		gpio_pin_set_dt(&led, 0);
+		printk("LED APAGADO\n");
+		k_sleep(K_MSEC(100));
+
+		gpio_pin_set_dt(&led, 1);
+		printk("LED ENCENDIDO\n");
+		k_sleep(K_MSEC(100));
+		gpio_pin_set_dt(&led, 0);
+		printk("LED APAGADO\n");
+
+		k_sleep(K_MSEC(SLEEP_TIME_MS));
+	}
 	return 0;
 }
